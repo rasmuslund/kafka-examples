82c95d8e029be9c3f10888a6f20c207f1d7357d8
Rasmus Lund <rasmus@ralu.dk>
2017-12-24 16:57:38 +0100

    Add admin example

---
+++ src/main/java/dk/ralu/examples/kafka/Admin.java
@@ -0,0 +1,57 @@
+package dk.ralu.examples.kafka;
+
+import java.util.Collection;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.DescribeClusterOptions;
+import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.common.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Admin {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Admin.class);
+
+    public static void main(String[] args) throws Exception {
+        Properties kafkaProps = new Properties();
+
+        kafkaProps.put("bootstrap.servers", "127.0.0.1:9092");
+
+        try (KafkaAdminClient kafkaAdminClient = (KafkaAdminClient) AdminClient.create(kafkaProps)) {
+            logClusterInfo(kafkaAdminClient);
+            logTopics(kafkaAdminClient);
+        }
+    }
+
+    private static void logTopics(KafkaAdminClient kafkaAdminClient) throws Exception {
+        ListTopicsResult listTopicsResult = kafkaAdminClient.listTopics(new ListTopicsOptions().listInternal(true));
+        LOGGER.info("Topics: {}", listTopicsResult.namesToListings().get());
+    }
+
+    private static void logClusterInfo(KafkaAdminClient kafkaAdminClient) throws Exception {
+        DescribeClusterResult describeClusterResult = kafkaAdminClient.describeCluster(new DescribeClusterOptions());
+        LOGGER.info("ClusterId: {}", describeClusterResult.clusterId().get());
+        LOGGER.info("Controller: {}", toString(describeClusterResult.controller().get()));
+        LOGGER.info("Nodes: {}", toString(describeClusterResult.nodes().get()));
+    }
+
+    private static String toString(Node node) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Node{");
+        sb.append("id:").append(node.idString());
+        sb.append(", host:").append(node.host());
+        sb.append(", port:").append(node.port());
+        sb.append(", rack:").append(node.rack());
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private static String toString(Collection<Node> nodes) {
+        return nodes.stream().map(Admin::toString).collect(Collectors.joining(", ", "{", "}"));
+    }
+}
